545570f63201e53ec66778dd3638d682f9c7171f
Jan Pamula <jaanpamula@gmail.com>
2021-11-01 01:50:38 +0100

    zadanie_data_procesing

--- 02-data-processing/exercise.js
+++ 02-data-processing/exercise.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { setMaxListeners } = require('process');
 
 function loadCsv(filePath) {
   const data = fs.readFileSync(filePath, 'utf8');
@@ -20,12 +21,32 @@ console.log('Count of codes in database: ', codes.length);
 // wyszukaj miast zaczunających się na jakiś ciąg znaków
 
 
+
+const ExampleCity = codes.filter((obj) => {
+  return obj.city.startsWith('A') === true;
+});
+//console.log(ExampleCity);
+
+
 // wyszukaj kodów zaczunających się ...
+
+const codeCity = codes.filter((obj) => {
+  return obj.code.startsWith('30-61') === true;
+});
+//console.log(codeCity);
 
 
 // wylistuj ilość miast w wojewudztwie
 
+const exampleRegion = codes.filter((obj) => {
+  return obj.region.startsWith('Województwo małopolskie') === true;
+});
+//console.log(exampleRegion);
 
 // wyswielt wszystkie miasta ktore nie są unikalne
 
+function checkUniq(el, index, self) {
+return self.indexOf(el) === index;}
 
+const uniq = codes.map(obj => obj.city).filter(checkUniq)
+console.log(uniq)
