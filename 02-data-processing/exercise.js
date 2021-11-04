5addd3524978679e951abe73b7f64acb3ca61f42
Jan Pamula <jaanpamula@gmail.com>
2021-11-04 20:02:00 +0100

    04.11.2021

--- 02-data-processing/exercise.js
+++ 02-data-processing/exercise.js
@@ -9,12 +9,64 @@ console.log('Count of codes in database: ', codes.length);
 // get part of array
 
 
-// pop
 
+// for (let i = 0; i<10; i++){
+//   console.log(codes[i])
+// }
+
+//console.log(codes.slice(5,10))
+
+//codes.slice(5,10).forEach(obj => console.log(obj))
+
+// for ( let el of codes.slice(5,10)){
+//   console.log(el)
+// }
+
+
+
+//acces of properties of object
+
+// for (let prop in codes[0]){
+//   console.log(prop, codes[0][prop])
+// }
+
+// console.log(Object.values(codes[0]))
+// console.log(Object.keys(codes[0]))
+// const obj = codes[0]
+// Object.keys(obj).forEach(prop => console.log(prop, obj[prop]))
+
+// pop pop, shift, splice
+function log(a){ console.log(a)};
+// log(codes.length) 
+// log(codes.pop())
+// log(codes.length)
+
+// log(codes.length) 
+// log(codes.shift())
+// log(codes.length)
+
+
+// log(codes.length) 
+// log(codes.splice(10, 10, "aAAAAAa"))
+// log(codes.length)
+// log(codes[10])
 
 // delete form array
 
+// log(codes[1])
+// log(codes.length) 
+// delete codes[1]
+// log(codes[1])
+// log(codes.length) 
 
+// log(codes[1])
+ 
+// delete codes[1]['city']
+// log(codes[1])
+
+const x = [{x: [{v: 1}]}]
+log(x[0]["x"][0]["v"])
+log(x[0].x[0].v)
 // add to array
 
 
