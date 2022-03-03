5f76c2a8ec544b974cf461b7af4ad69e05155f79
Miroslaw Kapinos <mkapinos@agileo.pl>
2022-03-03 16:23:31 +0100

    05-oop

---
+++ 05-oop/index.js
@@ -0,0 +1,14 @@
+const data = require("./data.json");
+console.log({data});
+
+// sprawdź ile ludzi mają na koncie kasy
+
+// programowanie strukturalne
+
+
+
+// programowanie funkcyjne
+
+
+
+// programowanie obiektowe
