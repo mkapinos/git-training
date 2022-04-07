c09a6c1b9bd0ed5dc4606f24f46244b93c73ac01
Kamila-Kapinos <s221572@student.uek.krakow.pl>
2022-04-07 20:12:48 +0200

    sql

---
+++ bankomat/bankomat_2022-04-07.sql
@@ -0,0 +1,110 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.7.3-MariaDB)
+# Database: bankomat
+# Generation Time: 2022-04-07 18:10:35 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table accounts
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `accounts`;
+
+CREATE TABLE `accounts` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(32) NOT NULL DEFAULT '',
+  `last_name` varchar(32) NOT NULL DEFAULT '',
+  `email` varchar(32) NOT NULL DEFAULT '',
+  `gender` enum('Male','Female','Non-binary') NOT NULL DEFAULT 'Non-binary',
+  `year-of-born` smallint(5) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+
+INSERT INTO `accounts` (`id`, `first_name`, `last_name`, `email`, `gender`, `year-of-born`)
+VALUES
+	(1,'Kamila','Kabanos','kamila@kabanos.pl','Female',2002),
+	(2,'Tomasz','Problem','tp@gmail.com','Male',1987),
+	(3,'Ala','Makota','makota@ala.com','Non-binary',2007),
+	(4,'Jakub','Morajda','jm@lol.pl','Male',1969);
+
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table cards
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `cards`;
+
+CREATE TABLE `cards` (
+  `number` bigint(16) unsigned NOT NULL,
+  `account_id` int(10) unsigned NOT NULL,
+  `pin` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`number`),
+  KEY `account_ref` (`account_id`),
+  CONSTRAINT `account_ref` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+LOCK TABLES `cards` WRITE;
+/*!40000 ALTER TABLE `cards` DISABLE KEYS */;
+
+INSERT INTO `cards` (`number`, `account_id`, `pin`)
+VALUES
+	(1234567890123456,1,5632);
+
+/*!40000 ALTER TABLE `cards` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table operations
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `operations`;
+
+CREATE TABLE `operations` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL,
+  `amount` double NOT NULL,
+  `account_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `op_ref` (`account_id`),
+  CONSTRAINT `op_ref` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+
+LOCK TABLES `operations` WRITE;
+/*!40000 ALTER TABLE `operations` DISABLE KEYS */;
+
+INSERT INTO `operations` (`id`, `date`, `amount`, `account_id`)
+VALUES
+	(1,'2022-03-04 18:00:00',-1500,1),
+	(2,'2022-05-02 14:00:00',-10000,1);
+
+/*!40000 ALTER TABLE `operations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
